b6d6269d7538c85ee5ce94ff03ea61d74b23eb12
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2024-02-29 21:47:09 +0000

    feat: star wars data modeling

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -27,6 +27,44 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+    
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+class character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    age = Column(Integer)
+    height = Column(Float)
+    gender = Column(String(250))
+    affiliation = Column(String(250))
+
+class planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    mass = Column(Float)
+    diameter = Column(Float)
+    atmosphere = Column(Float)
+    orbital_period_year = Column(Float)
+    orbital_period_day = Column(Float)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(character)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(planet)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
